0adb9999ef86faed8419459f4fab83c7f14e846b
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2023-04-09 21:41:10 +0900

    Initial Commit

---
+++ whisperBot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  whisperBot
+//
+//  Created by Yuta Nakamura on 2023/04/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
